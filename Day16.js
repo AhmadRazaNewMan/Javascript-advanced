a9cef442caca31116c26d01a97278d7e03ad972c
AhmadRazaNewMan <webdevolopmentbyar@example.com>
2024-04-17 14:42:39 +0500

    MinNumber In the array

---
+++ Day16.js
@@ -0,0 +1,8 @@
+console.log("Hello World its day 15 of our challenge");
+
+const minFinder = (arr)=>{
+    const minNumber = Math.min(...arr);
+   console.log(minNumber)
+}
+
+minFinder([11,2,3,4,5,5,6,7])
