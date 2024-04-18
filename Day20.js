fa42b642fb77c3d8f7131ed7fa9ca386d9c5c729
AhmadRazaNewMan <webdevolopmentbyar@example.com>
2024-04-18 16:22:07 +0500

    Adding new Reverse string function in the code

---
+++ Day20.js
@@ -0,0 +1,15 @@
+console.log('Day 20 and its a new challange')
+
+const rString = (str)=>{
+    var rString = ""
+    for  (let i = str.length-1; i >=0 ; i--) {
+        rString += str[i]
+        
+    }
+    console.log(rString)
+}
+
+
+
+
+rString("Hello")
