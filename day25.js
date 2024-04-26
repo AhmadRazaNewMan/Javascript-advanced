ef22dcebaeda2b118662d88d1e23e6de35db4845
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-04-26 10:06:41 +0500

    Adding day 25 task

---
+++ day25.js
@@ -0,0 +1,11 @@
+const countCharInString = (str) => {
+    let test_1 = str.split(" ").join("");
+    console.log(test_1.length);
+    let newString = "";
+    for (let i = test_1.length - 1; i >= 0; i--) {
+        newString += test_1[i];
+    }
+    console.log(newString+newString);
+}
+
+countCharInString("Ahmad Raza");
