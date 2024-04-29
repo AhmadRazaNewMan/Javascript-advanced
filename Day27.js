ff3f67e6fa06e24bebac2cffb86b1318cd4a27e7
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-04-29 16:30:32 +0500

    Adding new Task

---
+++ Day27.js
@@ -0,0 +1,13 @@
+const newSmallProject = (str,maxLength) =>{
+
+   let newString = ''
+
+    for(let i =0;i<maxLength;i++){
+
+        newString += str[i]
+
+}
+console.log(newString+"...")
+}
+
+newSmallProject("Ahmad's Raza New Proect",7)
---
+++ Day28.js
@@ -0,0 +1,5 @@
+
+
+
+let randomNumber = Math.floor(Math.random() * 11)+30;
+console.log(randomNumber)
